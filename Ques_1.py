f2a52bb99b03170bb0878b9172f454fc4eaf3c13
VijalDsouza <vijal.datagrokr.co>
2025-10-31 12:25:25 +0530

    Updated ques 1 using generator

--- Ques_1.py
+++ Ques_1.py
@@ -10,33 +10,19 @@ try:
 
     cursor = conn.cursor()
 
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS Employees (
-        ID INT PRIMARY KEY,
-        Name VARCHAR(100),
-        Age INT,
-        Department VARCHAR(100)
-    )
-    '''
+    read_query = "SELECT * FROM employees"
+    cursor.execute(read_query)
 
-    cursor.execute(create_table_query)
-    print("Employees Table created successfully.")
-
-    insert_query = " INSERT INTO Employees (ID, Name, Age, Department) VALUES (%s, %s, %s, %s) "
-    values = [
-        (1, 'Alice', 30, 'HR'),
-        (2, 'Bob', 25, 'Engineering'),
-        (3, 'Charlie', 28, 'Marketing')
-    ]
-
-    cursor.executemany(insert_query, values)
-    cursor.commit()
-    print("Records inserted successfully.")
-
-    cursor.execute("SELECT * FROM Employees")
-    for row in cursor.fetchall():
-        print(row)
-
+    def batch_processing():
+        while True:
+            rows = cursor.fetchmany(size=500)
+            if not rows:
+                break
+            for row in rows:
+                yield row
+    for row in batch_processing():
+        print(*row)
+    
 except mysql.connector.Error as err:
         print(f"MySQL Error: {err}")
 except Exception as e:
