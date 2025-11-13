6f8ed47ffb667860ba7b7902c459ed6d323b422f
VijalDsouza <vijal.datagrokr.co>
2025-11-13 10:21:50 +0530

    Modified ques 5 to show diff btw og and cleaned

--- Ques_5.py
+++ Ques_5.py
@@ -7,6 +7,10 @@ url = sys.argv[1]
 response = requests.get(url)
 html_content = response.text
 
+print("Original HTML Content:")
+print(html_content)
+
+print("\nCleaned Text Content:")
 soup = BeautifulSoup(html_content, 'html.parser')
 cleaned_text = soup.get_text()
 print(cleaned_text)
